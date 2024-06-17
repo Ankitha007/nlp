a4fd6208a5970e783f2a43784d8ca79d049ab20f
Ankitha007 <60316927+Ankitha007@users.noreply.github.com>
2024-06-17 13:23:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ custom_objects = {
 }
 
 with keras.utils.custom_object_scope(custom_objects):
-    transformer = keras.models.load_model('model.h5', compile=False)
+    transformer = tf.keras.models.load_model('model.h5', compile=False)
 
 # Load the vectorization layers
 with open(source_vector_path, 'rb') as f:
